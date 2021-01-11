c1199761b9eac1741a5d3ffefa50355cb8232e17
Vladislavkn <inova136@gmail.com>
2021-01-11 21:32:20 +0300

    add browsersync & update watch function

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
-import { src, dest, watch as watchFunction } from "gulp";
+import { src, dest, watch, parallel } from "gulp";
 import scss from "gulp-scss";
 import concat from "gulp-concat";
+import browserSyncFactory from "browser-sync";
 
 import path from "./path-config.js";
+
+const browserSync = browserSyncFactory.create();
+
+export const serve = () =>
+  browserSync.init({
+    server: { baseDir: path.dest.folder },
+  });
 
 export const styles = () =>
   src(path.src.styles)
     .pipe(scss({ outputStyle: "compressed" }))
     .pipe(concat(path.dest.stylesFileName))
-    .pipe(dest(path.dest.styles));
+    .pipe(dest(path.dest.styles))
+    .pipe(browserSync.stream());
 
-export const watch = () => watchFunction([path.src.styles], styles);
+export const watchFiles = () => {
+  watch([path.src.styles], styles);
+  watch([path.src.html]).on("change", browserSync.reload);
+};
+
+export default parallel(serve, watchFiles);
--- path-config.js
+++ path-config.js
@@ -4,8 +4,10 @@ const distFolder = "dist";
 export default {
   src: {
     styles: `${srcFolder}/styles/**/*.scss`,
+    html: `${distFolder}/**/*.html`,
   },
   dest: {
+    folder: `${distFolder}/`,
     styles: `${distFolder}/assets/styles`,
     stylesFileName: "style.min.css",
   },
